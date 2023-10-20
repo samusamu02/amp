b599e8a18a64a4ad33d2027e5efc72bf30f711c4
samusamu02 <83334413+samusamu02@users.noreply.github.com>
2023-10-20 17:19:28 +0900

    サウンドの再生

--- amp/main.cpp
+++ amp/main.cpp
@@ -2,17 +2,28 @@
 
 int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
 {
+	int testSound;
+	int testSoftSound;
+
 	ChangeWindowMode(true);
-	if (!DxLib_Init())
+	if (DxLib_Init() == -1)
 	{
 		return -1;
 	}
+
+	testSoftSound = LoadSoftSound(L"Sound/Peak_test_A.wav");
+	testSound = LoadSoundMemFromSoftSound(testSoftSound);
+
+	// ÉTÉEÉìÉhÇÃçƒê∂
+	PlaySoundMem(testSound, DX_PLAYTYPE_LOOP);
+
 	SetDrawScreen(DX_SCREEN_BACK);
 	while (ProcessMessage() == 0)
 	{
 		ScreenFlip();
 		ClearDrawScreen();
-	}
 
+	}
+	DxLib_End();
 	return 0;
 }
